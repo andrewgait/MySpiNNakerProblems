ee2ba8778189cdb840f20610625929b587c93737
Andrew Gait <andrew.gait@manchester.ac.uk>
2017-11-10 10:54:30 +0000

    Small test for running multiple short ss poisson runs

---
+++ user_problems/pynn0.8/test_spike_source_poisson_short_multirun.py
@@ -0,0 +1,14 @@
+import spynnaker8 as p
+
+p.setup(1)
+simtime = 100
+pop_src = p.Population(1, p.SpikeSourcePoisson(
+    rate=50, start=0, duration=simtime), label="src")
+pop_src.record("spikes")
+
+for i in range(simtime//2):
+    p.run(2)
+
+spikes = pop_src.get_data("spikes")
+p.end()
+print spikes.segments[0].spiketrains
