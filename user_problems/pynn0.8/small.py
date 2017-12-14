57c18343a96dc2e79a00e38de99305539e72d33d
Andrew Gait <andrew.gait@manchester.ac.uk>
2017-12-14 13:36:15 +0000

    Script to test run-reset-popchange-run

---
+++ user_problems/pynn0.8/small.py
@@ -0,0 +1,14 @@
+import spynnaker8 as sim
+
+sim.setup(timestep=1)
+
+pop_1 = sim.Population(5, sim.IF_curr_exp(), label="pop_1")
+simtime = 10
+sim.run(simtime)
+
+sim.reset()
+
+pop_2 = sim.Population(5, sim.IF_curr_exp(), label="pop_2")
+sim.run(simtime)
+
+sim.end()
