91e7fe55a84e9168f4eb3ae76fed55c49f77ba1b
Andrew Gait <andrew.gait@manchester.ac.uk>
2018-03-05 14:27:20 +0000

    Further playing with index-based connector script

--- user_problems/pynn0.8/indexbasedprob_script.py
+++ user_problems/pynn0.8/indexbasedprob_script.py
@@ -18,7 +18,7 @@ cell_params_lif = {'cm': 0.25,
 
 #p.set_number_of_neurons_per_core(p.IF_curr_exp, 50)
 
-n = 500 # 10
+n = 6 # 10
 # tau_exc = 1.0
 # tau_inh = 1.0
 weight_to_spike = 3.0
@@ -27,6 +27,7 @@ runtime = 200
 
 # Network
 pop_1 = p.Population(n, p.IF_curr_exp(**cell_params_lif), label='pop_1')
+pop_2 = p.Population(n/2, p.IF_curr_exp(**cell_params_lif), label='pop_2')
 
 # SpikeInjector
 injectionConnection = [(0, n/2)]
@@ -38,6 +39,7 @@ p.Projection(inj_pop, pop_1, p.FromListConnector(injectionConnection),
 
 # Connectors
 index_based_exc = "(i+j)/"+str(n*(n-n/2.0))
+index_based_exc2 = "2.0*(i+j)/"+str(n*(n-n/2.0))
 index_based_inh = "(i+j)/"+str(n*(n+n/2.0))
 print 'index_based_exc: ', index_based_exc
 print 'index_based_inh: ', index_based_inh
@@ -46,19 +48,27 @@ exc_connector = p.IndexBasedProbabilityConnector(
     index_based_exc, allow_self_connections=True)
 inh_connector = p.IndexBasedProbabilityConnector(
     index_based_inh, allow_self_connections=False)
+exc_connector2 = p.IndexBasedProbabilityConnector(
+    index_based_exc2, allow_self_connections=True)
 
 # Wire grid
 p.Projection(pop_1, pop_1, exc_connector,
              p.StaticSynapse(weight=2.0, delay=5))
 p.Projection(pop_1, pop_1, inh_connector,
-             p.StaticSynapse(weight=0.5, delay=10))
+             p.StaticSynapse(weight=1.5, delay=5))
+
+p.Projection(pop_1, pop_2, exc_connector2,
+             p.StaticSynapse(weight=1.5, delay=2))
 
 pop_1.record(['v','spikes'])
+pop_2.record(['v','spikes'])
 
 p.run(runtime)
 
 v = pop_1.get_data('v')
 spikes = pop_1.get_data('spikes')
+v2 = pop_2.get_data('v')
+spikes2 = pop_2.get_data('spikes')
 
 Figure(
     # raster plot of the presynaptic neurons' spike times
@@ -66,9 +76,16 @@ Figure(
           yticks=True, markersize=2.0, xlim=(0, runtime), xticks=True),
     # membrane potential of the postsynaptic neurons
     Panel(v.segments[0].filter(name='v')[0],
-          ylabel="Membrane potential (mV)",
+          ylabel="Membrane potential pop 1 (mV)",
           data_labels=[pop_1.label], yticks=True, xlim=(0, runtime), xticks=True),
-    title="Simple index-based prob connector",
+    # raster plot of the presynaptic neurons' spike times
+    Panel(spikes2.segments[0].spiketrains,
+          yticks=True, markersize=2.0, xlim=(0, runtime), xticks=True),
+    # membrane potential of the postsynaptic neurons
+    Panel(v2.segments[0].filter(name='v')[0],
+          ylabel="Membrane potential pop 2 (mV)",
+          data_labels=[pop_2.label], yticks=True, xlim=(0, runtime), xticks=True),
+    title="Simple index-based probability connector",
     annotations="Simulated with {}".format(p.name())
 )
 plt.show()
