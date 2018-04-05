a8ec1da58e0e6beaaafc670c7a104a01a97308c8
Andrew Gait <andrew.gait@manchester.ac.uk>
2018-04-05 15:09:16 +0100

    More commands added.
    - don't know why gplotsel2d command is erroring

--- user_problems/test_csa_commands.py
+++ user_problems/test_csa_commands.py
@@ -7,9 +7,16 @@ def GvspaceToCx(p):
     return (w.real, w.imag)
 
 
+g = random2d(900)
+gplot2d(g, 900)
+
+g = grid2d(30)
+d = euclidMetric2d(g)
+r = 2
+c = disc(r)*d
+#gplotsel2d(g,c,434)
+
 g1 = grid2d(30)
 g2 = grid2d(30, x0=-7.0, xScale=8.0, yScale=8.0)
 
 gplot2d(GvspaceToCx * g1, 900)
-
-
