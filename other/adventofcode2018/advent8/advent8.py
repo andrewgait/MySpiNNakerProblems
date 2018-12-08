7e8496dfc0b7a457b1720584af0762379ed1a839
Andrew Gait <andrew.gait@manchester.ac.uk>
2018-12-08 12:04:07 +0000

    solved part 1 day 8

--- other/adventofcode2018/advent8/advent8.py
+++ other/adventofcode2018/advent8/advent8.py
@@ -4,6 +4,7 @@
 #input = open("advent8_input.txt", "r")
 input = open("advent8_test_input.txt", "r")
 
+data = []
 # read string into array
 for line in input:
     # count the number of spaces
@@ -14,22 +15,28 @@ for line in input:
 size = len(data)
 print(size)
 
-def read_header_and_sum(data, sum):
-    print(data)
+node_val_array = []
+
+def read_header_and_sum():
+    global data
+    global sum
+    global node_val_array
+#    print(data)
     children = int(data[0])
     entries = int(data[1])
     data = data[2:]
+
     for i in range(children):
-        sum += read_header_and_sum(data, sum)
+        read_header_and_sum()
 
     for j in range(entries):
         sum += int(data[j])
 
     print('sum: ', sum)
     data = data[entries:]
-    return sum
 
 sum = 0
-total = read_header_and_sum(data, sum)
+read_header_and_sum()
 
-print('sum total is ', total)
+print('sum total is ', sum)
+print('len(data) is ', len(data))
