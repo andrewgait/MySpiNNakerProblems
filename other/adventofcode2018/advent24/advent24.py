db95150c0fa5d852495a1c473f76be058faecc7d
Andrew Gait <andrew.gait@manchester.ac.uk>
2018-12-21 19:22:32 +0000

    deleted adventofcode stuff, moved it to its own repository

--- other/adventofcode2018/advent24/advent24.py
+++
@@ -1,7 +0,0 @@
-# Advent of code, day 24
-
-# open file
-input = open("advent24_input.txt", "r")
-
-# read string into array
-for line in input:
--- other/adventofcode2018/advent25/advent25.py
+++
@@ -1,7 +0,0 @@
-# Advent of code, day 25
-
-# open file
-input = open("advent25_input.txt", "r")
-
-# read string into array
-for line in input:
