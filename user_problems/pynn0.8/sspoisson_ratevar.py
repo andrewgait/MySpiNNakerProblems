75b87abcb2a9d5114b68c655f6ccbf195625a2fa
Andrew Gait <andrew.gait@manchester.ac.uk>
2019-05-30 14:41:28 +0100

    add log plots to sspoisson ratevar

--- user_problems/pynn0.8/sspoisson_ratevar.py
+++ user_problems/pynn0.8/sspoisson_ratevar.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as pylab
 import statistics
+import math
 import spynnaker8 as sim
 
 rates = [0, 0.0001, 0.0005, 0.001, 0.002, 0.005, 0.007, 0.01, 0.02,
@@ -10,9 +11,10 @@ rates = [0, 0.0001, 0.0005, 0.001, 0.002, 0.005, 0.007, 0.01, 0.02,
 
 # rates = [2000, 3000]
 n_neurons = 20 # number of neurons in each population
-simtime = 10 * 10000
+tstep = 1.0
+simtime = 100 * 1000 * tstep
 
-sim.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
+sim.setup(timestep=tstep, min_delay=1.0*tstep, max_delay=144.0*tstep)
 # sim.setup(timestep=0.1, min_delay=0.1, max_delay=14.4)
 
 inputs = {}
@@ -46,21 +48,40 @@ sim.end()
 
 means_plot = []
 vars_plot = []
+means_log_plot = []
+vars_log_plot = []
 for rate in rates:
     print(rate, means[rate], vars[rate])
     means_plot.append(means[rate])
     vars_plot.append(vars[rate])
-
+    means_log_plot.append(math.log(means[rate]) if means[rate]!=0 else 0)
+    vars_log_plot.append(math.log(vars[rate]) if vars[rate]!=0 else 0)
 
 # plot means against variances
-pylab.subplot(2, 1, 1)
+pylab.subplot(2, 2, 1)
 pylab.xlabel("Means")
 pylab.ylabel("Variances")
+pylab.title("mean vs variance, all")
 pylab.plot(means_plot, vars_plot, 'ro-')
 
-pylab.subplot(2, 1, 2)
+pylab.subplot(2, 2, 2)
 pylab.xlabel("Means")
 pylab.ylabel("Variances")
+pylab.title("low end values")
 pylab.plot(means_plot[0:15], vars_plot[0:15], 'ro-')
+
+pylab.subplot(2, 2, 3)
+pylab.xlabel("Means")
+pylab.ylabel("Variances")
+pylab.title("log, mean vs variance, all")
+pylab.plot(means_log_plot, vars_log_plot, 'bo')
+
+pylab.subplot(2, 2, 4)
+pylab.xlabel("Means")
+pylab.ylabel("Variances")
+pylab.title("log, low end values")
+pylab.plot(means_log_plot[0:15], vars_log_plot[0:15], 'bo')
+
+pylab.subplots_adjust(wspace=0.5, hspace=0.5)
 
 pylab.show()
