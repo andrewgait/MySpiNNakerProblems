07c882440af65d4860b76c9725804c7b685f7f1a
Andrew Gait <andrew.gait@manchester.ac.uk>
2019-06-25 13:38:52 +0100

    Add plotting of connections to fixed-pre and fixed-post

--- user_problems/pynn0.8/test_fixed_pre_simpler.py
+++ user_problems/pynn0.8/test_fixed_pre_simpler.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 sim.setup(timestep=1.0)
 
-n_pre_neurons = 6
-n_post_neurons = 6
-n_pop2 = 10
-sim.set_number_of_neurons_per_core(sim.IF_curr_exp, 4)  # n_post_neurons // 2)
+n_pre_neurons = 60
+n_post_neurons = 80
+n_pop2 = 100
+# sim.set_number_of_neurons_per_core(sim.IF_curr_exp, 4)  # n_post_neurons // 2)
 
 runtime = 100
 
@@ -17,9 +17,9 @@ pop = sim.Population(n_post_neurons, sim.IF_curr_exp(), label="pop")
 input_pop2 = sim.Population(n_pre_neurons, sim.SpikeSourceArray([0]), label="input2")
 pop2 = sim.Population(n_pop2, sim.IF_curr_exp(), label="pop2")
 
-pop3 = sim.Population(n_post_neurons, sim.IF_curr_exp(), label="pop2")
+pop3 = sim.Population(n_post_neurons, sim.IF_curr_exp(), label="pop3")
 
-n_pre = 2
+n_pre = 10
 weights = 3.0
 delays = 5
 
@@ -47,7 +47,7 @@ c5 = sim.Projection(pop3, pop3,
                                                 allow_self_connections=False),
                     sim.StaticSynapse(weight=weights, delay=delays))
 
-pop.record(['v', 'spikes'])
+pop3.record(['v', 'spikes'])
 
 sim.run(runtime)
 
@@ -66,26 +66,26 @@ weights_delays4 = sorted(c4.get(['weight', 'delay'], 'list'),
 weights_delays5 = sorted(c5.get(['weight', 'delay'], 'list'),
                          key = lambda x: x[1])
 
-print(weights_delays)
+# print(weights_delays)
 print(len(weights_delays))
-print(weights_delays2)
+# print(weights_delays2)
 print(len(weights_delays2))
-print(weights_delays3)
+# print(weights_delays3)
 print(len(weights_delays3))
-print(weights_delays4)
+# print(weights_delays4)
 print(len(weights_delays4))
-print(weights_delays5)
+# print(weights_delays5)
 print(len(weights_delays5))
 
 # get data (could be done as one, but can be done bit by bit as well)
-spikes = pop.get_data('spikes')
-v = pop.get_data('v')
+spikes = pop3.get_data('spikes')
+v = pop3.get_data('v')
 
 Figure(
     # membrane potential of the postsynaptic neuron
     Panel(v.segments[0].filter(name='v')[0],
           ylabel="Membrane potential (mV)",
-          data_labels=[pop.label], yticks=True, xlim=(0, runtime)),
+          data_labels=[pop3.label], yticks=True, xlim=(0, runtime)),
     # raster plot of the presynaptic neuron spike times
     Panel(spikes.segments[0].spiketrains,
           xlabel="Time (ms)", xticks=True,
@@ -93,6 +93,56 @@ Figure(
     title="fixed-number-pre connector test",
     annotations="Simulated with {}".format(sim.name())
 )
+
+plt.show()
+
+# plot the connections for fun
+plt.subplot(2, 3, 1)
+plt.xlabel("pre")
+plt.ylabel("post")
+plt.title("pre-post connections, n_pre={}, n_post={}".format(n_pre_neurons,
+                                                             n_post_neurons))
+plt.plot(list(weights_delays[n][0] for n in range(len(weights_delays))),
+         list(weights_delays[n][1] for n in range(len(weights_delays))),
+         'ro', markersize=0.5)
+
+plt.subplot(2, 3, 2)
+plt.xlabel("pre")
+plt.ylabel("post")
+plt.title("pre-post connections, n_pre={}, n_post={}".format(n_pre_neurons,
+                                                             n_pop2))
+plt.plot(list(weights_delays2[n][0] for n in range(len(weights_delays2))),
+         list(weights_delays2[n][1] for n in range(len(weights_delays2))),
+         'ro', markersize=0.5)
+
+plt.subplot(2, 3, 3)
+plt.xlabel("pre")
+plt.ylabel("post")
+plt.title("pre-post connections, n_pre={}, n_post={}".format(n_pop2,
+                                                             n_pop2))
+plt.plot(list(weights_delays3[n][0] for n in range(len(weights_delays3))),
+         list(weights_delays3[n][1] for n in range(len(weights_delays3))),
+         'ro', markersize=0.5)
+
+plt.subplot(2, 3, 4)
+plt.xlabel("pre")
+plt.ylabel("post")
+plt.title("pre-post connections, n_pre={}, n_post={}".format(n_pre_neurons,
+                                                             n_post_neurons))
+plt.plot(list(weights_delays4[n][0] for n in range(len(weights_delays4))),
+         list(weights_delays4[n][1] for n in range(len(weights_delays4))),
+         'ro', markersize=0.5)
+
+plt.subplot(2, 3, 5)
+plt.xlabel("pre")
+plt.ylabel("post")
+plt.title("pre-post connections, n_pre={}, n_post={}".format(n_post_neurons,
+                                                             n_post_neurons))
+plt.plot(list(weights_delays5[n][0] for n in range(len(weights_delays5))),
+         list(weights_delays5[n][1] for n in range(len(weights_delays5))),
+         'ro', markersize=0.5)
+
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
 
 plt.show()
 
