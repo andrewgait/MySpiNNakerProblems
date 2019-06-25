c33d75c6bec885b52d7f9c5d85090e07209bf0a3
Andrew Gait <andrew.gait@manchester.ac.uk>
2019-06-25 10:43:37 +0100

    Update fixed-pre script

--- user_problems/pynn0.8/test_fixed_pre_simpler.py
+++ user_problems/pynn0.8/test_fixed_pre_simpler.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 sim.setup(timestep=1.0)
 
-n_pre_neurons = 4
-n_post_neurons = 4
+n_pre_neurons = 6
+n_post_neurons = 6
 n_pop2 = 10
 sim.set_number_of_neurons_per_core(sim.IF_curr_exp, 4)  # n_post_neurons // 2)
 
@@ -14,10 +14,10 @@ runtime = 100
 input_pop = sim.Population(n_pre_neurons, sim.SpikeSourceArray([0]), label="input")
 pop = sim.Population(n_post_neurons, sim.IF_curr_exp(), label="pop")
 
-# input_pop2 = sim.Population(n_pre_neurons, sim.SpikeSourceArray([0]), label="input2")
-# pop2 = sim.Population(n_pop2, sim.IF_curr_exp(), label="pop2")
-#
-# pop3 = sim.Population(n_post_neurons, sim.IF_curr_exp(), label="pop2")
+input_pop2 = sim.Population(n_pre_neurons, sim.SpikeSourceArray([0]), label="input2")
+pop2 = sim.Population(n_pop2, sim.IF_curr_exp(), label="pop2")
+
+pop3 = sim.Population(n_post_neurons, sim.IF_curr_exp(), label="pop2")
 
 n_pre = 2
 weights = 3.0
@@ -26,26 +26,26 @@ delays = 5
 c1 = sim.Projection(input_pop, pop, sim.FixedNumberPreConnector(n_pre),
                     sim.StaticSynapse(weight=weights, delay=delays))
 
-# c2 = sim.Projection(input_pop2, pop2,
-#                     sim.FixedNumberPreConnector(n_pre,
-#                                                 with_replacement=True),
-#                     sim.StaticSynapse(weight=weights, delay=delays))
-#
-# c3 = sim.Projection(pop2, pop2,
-#                     sim.FixedNumberPreConnector(n_pre,
-#                                                 with_replacement=True,
-#                                                 allow_self_connections=False),
-#                     sim.StaticSynapse(weight=weights, delay=delays))
-#
-# c4 = sim.Projection(input_pop2, pop3,
-#                     sim.FixedNumberPreConnector(n_pre,
-#                                                 allow_self_connections=False),
-#                     sim.StaticSynapse(weight=weights, delay=delays))
-#
-# c5 = sim.Projection(pop3, pop3,
-#                     sim.FixedNumberPreConnector(n_pre,
-#                                                 allow_self_connections=False),
-#                     sim.StaticSynapse(weight=weights, delay=delays))
+c2 = sim.Projection(input_pop2, pop2,
+                    sim.FixedNumberPreConnector(n_pre,
+                                                with_replacement=True),
+                    sim.StaticSynapse(weight=weights, delay=delays))
+
+c3 = sim.Projection(pop2, pop2,
+                    sim.FixedNumberPreConnector(n_pre,
+                                                with_replacement=True,
+                                                allow_self_connections=False),
+                    sim.StaticSynapse(weight=weights, delay=delays))
+
+c4 = sim.Projection(input_pop2, pop3,
+                    sim.FixedNumberPreConnector(n_pre,
+                                                allow_self_connections=False),
+                    sim.StaticSynapse(weight=weights, delay=delays))
+
+c5 = sim.Projection(pop3, pop3,
+                    sim.FixedNumberPreConnector(n_pre,
+                                                allow_self_connections=False),
+                    sim.StaticSynapse(weight=weights, delay=delays))
 
 pop.record(['v', 'spikes'])
 
@@ -54,28 +54,28 @@ sim.run(runtime)
 weights_delays = sorted(c1.get(['weight', 'delay'], 'list'),
                         key = lambda x: x[1])
 
-# weights_delays2 = sorted(c2.get(['weight', 'delay'], 'list'),
-#                          key = lambda x: x[1])
-#
-# weights_delays3 = sorted(c3.get(['weight', 'delay'], 'list'),
-#                          key = lambda x: x[1])
-#
-# weights_delays4 = sorted(c4.get(['weight', 'delay'], 'list'),
-#                          key = lambda x: x[1])
-#
-# weights_delays5 = sorted(c5.get(['weight', 'delay'], 'list'),
-#                          key = lambda x: x[1])
+weights_delays2 = sorted(c2.get(['weight', 'delay'], 'list'),
+                         key = lambda x: x[1])
+
+weights_delays3 = sorted(c3.get(['weight', 'delay'], 'list'),
+                         key = lambda x: x[1])
+
+weights_delays4 = sorted(c4.get(['weight', 'delay'], 'list'),
+                         key = lambda x: x[1])
+
+weights_delays5 = sorted(c5.get(['weight', 'delay'], 'list'),
+                         key = lambda x: x[1])
 
 print(weights_delays)
 print(len(weights_delays))
-# print(weights_delays2)
-# print(len(weights_delays2))
-# print(weights_delays3)
-# print(len(weights_delays3))
-# print(weights_delays4)
-# print(len(weights_delays4))
-# print(weights_delays5)
-# print(len(weights_delays5))
+print(weights_delays2)
+print(len(weights_delays2))
+print(weights_delays3)
+print(len(weights_delays3))
+print(weights_delays4)
+print(len(weights_delays4))
+print(weights_delays5)
+print(len(weights_delays5))
 
 # get data (could be done as one, but can be done bit by bit as well)
 spikes = pop.get_data('spikes')
