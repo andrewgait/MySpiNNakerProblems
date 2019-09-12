f04fa7e4c47b541a98d07ef237e5c6f3c3fb929d
Andrew Gait <andrew.gait@manchester.ac.uk>
2019-09-12 14:59:36 +0100

    moved a load of scripts into a more sensibly named directory higher up

--- my_testing/test_projection_slice.py
+++ my_testing/test_projection_slice.py
@@ -2,12 +2,12 @@ import spynnaker8 as sim
 from pyNN.utility.plotting import Figure, Panel
 import matplotlib.pyplot as plt
 
-sources = 260 # 260
-destinations = 260 # 260
+sources = 260
+destinations = 260
 
 sim.setup(timestep=1.0)
-# sim.set_number_of_neurons_per_core(sim.SpikeSourceArray, 5)
-# sim.set_number_of_neurons_per_core(sim.IF_curr_exp, 5)
+# sim.set_number_of_neurons_per_core(sim.SpikeSourceArray, 200)
+# sim.set_number_of_neurons_per_core(sim.IF_curr_exp, 200)
 
 pop1 = sim.Population(sources, sim.SpikeSourceArray(spike_times=[10,50]),
                       label="input")
@@ -15,20 +15,20 @@ pop2 = sim.Population(destinations, sim.IF_curr_exp(),
                       label="pop2")
 synapse_type = sim.StaticSynapse(weight=5.5, delay=2)
 
-pop1view = pop1[1:4]
-pop2view = pop2[1:4]
+# pop1view = pop1[250:258]
+# pop2view = pop2[250:258]
+pop1view = pop1[1:6]
+pop2view = pop2[253:258]
 
 pop2view.set(i_offset=0.1)
 
 connector = sim.OneToOneConnector()
 # connector = sim.AllToAllConnector()
-# connector = sim.FromListConnector([(1, 2), (0, 1)])
 # projection = sim.Projection(pop1, pop2, connector,
 #                             synapse_type=synapse_type)
 projection = sim.Projection(pop1view, pop2view, connector,
                             synapse_type=synapse_type)
-#projection = sim.Projection(pop1, pop2, sim.FromListConnector([[0,0]]),
-#                            synapse_type=synapse_type)
+
 # sim.run(0)
 
 pop2.record('all')
